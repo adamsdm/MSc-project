0bb6112911d0b3580b4bb3276a1720dfc0c1b9fc
Adam Söderström <adamsdm1@gmail.com>
2018-03-12 13:08:39 +0100

    Implemented upd pos with DC

--- src/4. DirectCompute/Shaders/UpdPos.hlsl
+++ src/4. DirectCompute/Shaders/UpdPos.hlsl
@@ -6,6 +6,12 @@ typedef struct
 	float weight;
 } Particle;
 
+cbuffer CB : register(b0)
+{
+	int MAX_PARTICLES;
+	float dt;
+	float simspeed;
+}
 
 RWBuffer<float>					positions	: register(u0);
 RWStructuredBuffer<Particle>	particles	: register(u1);
@@ -13,15 +19,14 @@ RWStructuredBuffer<Particle>	particles	: register(u1);
 [numthreads(1024, 1, 1)]
 void CSMain(uint3 DTid : SV_DispatchThreadID)
 {
-
-	float simspeed = 0.1;
-	float dt = 0.1;
+	
 
 	Particle p = particles[DTid.x];
 
 	p.px += p.vx * simspeed * dt;
 	p.py += p.vy * simspeed * dt;
 	p.pz += p.vz * simspeed * dt;
+
 
 	positions[DTid.x * 3 + 0] = p.px;
 	positions[DTid.x * 3 + 1] = p.py;
