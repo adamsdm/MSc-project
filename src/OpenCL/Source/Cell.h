971b966cef0a31bb223800d659aa620b57f1174d
Adam Söderström <adamsdm1@gmail.com>
2018-03-15 16:03:07 +0100

    Small refactor, moved common files to common folder

--- src/OpenCL/Source/Cell.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct Cell {
-	float m;
-	float com_x;
-	float com_y;
-	float com_z;
-};
--- src/OpenCL/Source/Particle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct
-{
-	float px, py, pz;
-	float vx, vy, vz;
-	float Fx, Fy, Fz;
-	float weight;
-} Particle;
